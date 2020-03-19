5987353de8cdd1d8e300850b2d8a05c801569cee
Martin Hudik <hudikm@gmail.com>
2020-03-19 15:14:24 +0100

    1.2 Zmena textu v prvku TextView

--- app/src/main/java/sk/uniza/activitytutorial/MainActivity.java
+++ app/src/main/java/sk/uniza/activitytutorial/MainActivity.java
@@ -1,14 +1,25 @@
 package sk.uniza.activitytutorial;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Ahoj Svet");
+            }
+        });
     }
 }
