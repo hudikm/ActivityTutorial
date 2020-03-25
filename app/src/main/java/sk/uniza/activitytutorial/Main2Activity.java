d07a3c241327466c2b698362ea57b023476eff76
Martin Hudik <hudikm@gmail.com>
2020-03-25 17:23:36 +0100

    2.0 Pridaná nová aktivita

---
+++ app/src/main/java/sk/uniza/activitytutorial/Main2Activity.java
@@ -0,0 +1,14 @@
+package sk.uniza.activitytutorial;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+    //    Nová aktivyta, pretože ľščťžýá
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
